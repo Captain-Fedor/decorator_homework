3997bd20a580944833e9ee484c456bb0ca891ed3
Fedor <runway067@gmail.com>
2024-08-01 19:56:50 +0300

    second commit

--- decor_homework.py
+++ decor_homework.py
@@ -1,13 +1,14 @@
 import logging
 import pathlib
+import os
 import requests
 
 """TASK 1"""
 
 
 def logging_1(func):
-    # logging.basicConfig(level=logging.INFO, filename='main_1.log',
-    #                     format='%(asctime)s %(levelname)s:%(message)s')
+    logging.basicConfig(level=logging.INFO, filename='main.log',
+                        format='%(asctime)s %(levelname)s:%(message)s')
 
     def wrapper(*args, **kwargs):
         result = str(func(*args, **kwargs))
@@ -29,7 +30,7 @@ def my_func_1(*args):
 
 def logging_2(arg):
     def inner_decorator(func):
-        logging.basicConfig(level=logging.INFO, filename='main_1.log',
+        logging.basicConfig(level=logging.INFO, filename='main.log',
                             format='%(asctime)s %(levelname)s:%(message)s')
 
         def wrapped(*args, **kwargs):
@@ -43,11 +44,11 @@ def logging_2(arg):
     return inner_decorator
 
 
-def get_filepath():
-    return f'"{str(pathlib.Path(__file__))}"'
+def get_filepath(file_name):
+    return os.path.abspath(file_name)
 
 
-@logging_2(get_filepath())
+@logging_2(get_filepath('main.log'))
 def my_func_2(*args):
     return sum(args)
 
@@ -74,4 +75,5 @@ my_func_1(10, 30, 40)
 my_func_2(1, 3, 4)
 
 id_list = [1, 2, 3]
+
 get_the_smartest_superhero(id_list)
